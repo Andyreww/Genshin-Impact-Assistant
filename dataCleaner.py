0d48a1a4d1cc311c139a84acb092714e9a7faefd
Andyreww <angulo_a1@denison.edu>
2024-03-01 14:16:39 -0500

    Cleaned Code but still fixing issue

--- dataCleaner.py
+++ dataCleaner.py
@@ -1,41 +1,40 @@
 import assist_Functions as aF
 import pandas as pd
 import asyncio
-import pprint
 from tabulate import tabulate
 
-# Pretty Print to make
-# Dictionary more Readable
-pp = pprint.PrettyPrinter(indent=2)
+def swap_rows(df, i1, i2, row): # Possible Error
+# Swap the names of the first and second artifacts
+    df = df.loc[i1, row], df.loc[i2, row] = df.loc[i2, row], df.loc[i1, row]
+    return df
 
-# Asking the user for basic information
-# Only for testing (Will Delete Later)
-uid = int(input("What is your in-game UID: "))
-character_Name = str(input(f"Hello {asyncio.run(aF.userName(uid))} what character are you requesting: "))
+def main():
+    # Asking the user for basic information
+    uid = int(input("What is your in-game UID: "))
+    character_Name = str(input(f"Hello {asyncio.run(aF.userName(uid))}, what character are you requesting: "))
 
-# Utilizing assist_Functions.py to sort all the data into readable tables
-asyncio.run(aF.userInfo(uid))
-characterDict = asyncio.run(aF.artifact_Extractor(uid, character_Name))
+    # Utilizing assist_Functions.py to sort all the data into readable tables
+    asyncio.run(aF.userInfo(uid))
+    characterDict = asyncio.run(aF.artifact_Extractor(uid, character_Name))
 
-# Creating Artifact DataFrame
-artifact_Retrieve = characterDict[character_Name]
+    # Creating Artifact DataFrame
+    artifact_Retrieve = characterDict[character_Name]
+    ArtifactsDF = pd.DataFrame(artifact_Retrieve, columns=['Artifacts'])
+    print(tabulate(ArtifactsDF, headers='keys', tablefmt='pretty'))
 
+    # Creating Stats Data Frame
+    mainStat_Retireve = asyncio.run(aF.artifact_MainStat(uid, character_Name))
+    mainStatDF = pd.DataFrame(mainStat_Retireve, columns=['Stat', 'Value'])
+    print(tabulate(mainStatDF, headers='keys', tablefmt='pretty'))
 
-# Issue on the order this is printing out
-# Will cause issues when assigning data
-# to the dataframe
-print(artifact_Retrieve)
-print([key for key in artifact_Retrieve])
-ArtifactsDF = pd.DataFrame(artifact_Retrieve, columns=['Artifacts'])# Print the DataFrame
-print(tabulate(ArtifactsDF, headers='keys', tablefmt='pretty'))
-
-# Creating Stats Data Frame
-# Main Stat DF
-mainStat_Retireve = asyncio.run(aF.artifact_MainStat(uid, character_Name)) #Created Workaround
-
-mainStatDF = pd.DataFrame(mainStat_Retireve, columns=['Stat', 'Value'])
-print(tabulate(mainStatDF, headers='keys', tablefmt='pretty'))
-#Now need to seperate List of Tuple into a pandas DF into seperate columns
+    # Merging the df
+    # Still fixing issue here
+    mergedDF = pd.concat([ArtifactsDF.reset_index(drop=True), mainStatDF.reset_index(drop=True)], axis=1)
+    mergeDF = swap_rows(mergeDF, 1, 4, 'Artifacts')
+    print(tabulate(mergedDF, headers='keys', tablefmt='pretty'))
 
 
 
+
+if __name__ == "__main__":
+    main()
