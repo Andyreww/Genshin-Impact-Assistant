dc8320f9e785c99e73454baee43f5833c45b8f48
Andyreww <angulo_a1@denison.edu>
2024-06-08 11:27:26 -0400

    all information regarding artifact/weapon in a df

--- dataCleaner.py
+++ dataCleaner.py
@@ -1,37 +1,117 @@
 import assist_Functions as aF
 import pandas as pd
 import asyncio
+import ast
 from tabulate import tabulate
 
-def swap_rows(df, i1, i2, row): # Possible Error
-# Swap the names of the first and second artifacts
-    df = df.loc[i1, row], df.loc[i2, row] = df.loc[i2, row], df.loc[i1, row]
-    return df
-
 def main():
-    # Asking the user for basic information
-    uid = int(input("What is your in-game UID: "))
-    character_Name = str(input(f"Hello {asyncio.run(aF.userName(uid))}, what character are you requesting: "))
+    uid = input("What is your in-game UID: ")
+    if(len(uid) > 9 or not uid.isnumeric()):
+        print("Not a Valid UID")
+        return -1
+    
+    character = input("What character are you requesting for: ")
 
-    # Utilizing assist_Functions.py to sort all the data into readable tables
-    asyncio.run(aF.userInfo(uid))
-    characterDict = asyncio.run(aF.artifact_Extractor(uid, character_Name))
+    # Creating Pandas Dataframe to handle the data better
+    
+    # Creating Player Table
+    uidDF = pd.DataFrame([uid], columns=['UID'])
+    uidDF['UID'] = uidDF['UID']
+    print(tabulate(uidDF, headers='keys', tablefmt='psql'))
+    #------------------------------------------------------------#
 
-    # Creating Artifact DataFrame
-    artifact_Retrieve = characterDict[character_Name]
-    ArtifactsDF = pd.DataFrame(artifact_Retrieve, columns=['Artifacts'])
-    print(tabulate(ArtifactsDF, headers='keys', tablefmt='pretty'))
+    # Creating Player Info Table
+    playerInfo = asyncio.run(aF.userInfo(uid))
+    lines = playerInfo.split('\n')[2:-2]
 
-    # Creating Stats Data Frame
-    mainStat_Retireve = asyncio.run(aF.artifact_MainStat(uid, character_Name))
-    mainStatDF = pd.DataFrame(mainStat_Retireve, columns=['Stat', 'Value'])
-    print(tabulate(mainStatDF, headers='keys', tablefmt='pretty'))
+    # Create a dictionary from the lines
+    data = {}
+    for line in lines:
+        key, value = line.split(': ')
+        if key == 'Characters':
+            value = ast.literal_eval(value)  # Convert string representation of list to actual list
+        elif key in ['Level', 'Achievements', 'Abyss Floor']:
+            value = int(value)  # Convert string representation of integer to actual integer
+        data[key] = [value]
+    
+    playerInfoDF = pd.DataFrame(data)
 
-    # Merging the df
-    # Still fixing issue here
-    mergedDF = pd.concat([ArtifactsDF.reset_index(drop=True), mainStatDF.reset_index(drop=True)], axis=1)
-    mergeDF = swap_rows(mergeDF, 1, 4, 'Artifacts')
-    print(tabulate(mergedDF, headers='keys', tablefmt='pretty'))
+    # Converting data into cleaned DataFrame
+    playerInfoDF = playerInfoDF.T
+    playerInfoDF.columns = ['Player Info']
+
+    print(tabulate(playerInfoDF, headers='keys', tablefmt='psql'))
+    #------------------------------------------------------------#
+
+    artifactInfo = asyncio.run(aF.artifact_Extractor(uid, character))
+
+    rows = []
+    for key, value in artifactInfo.items():
+        if isinstance(value, set):
+            for item in value:
+                rows.append({'Character': character, 'Artifact': item})
+        elif isinstance(value, list):
+            if all(isinstance(i, tuple) for i in value):
+                for stat, amount in value:
+                    rows.append({'Character': character, 'Artifact': key, 'Stat': stat, 'Amount': amount})
+            else:
+                stat = value[0]
+                amount = value[1] if len(value) > 1 else None  # Check if value has more than one element
+                rows.append({'Character': character, 'Artifact': key, 'Stat': stat, 'Amount': amount})
+
+    artifactDF = pd.DataFrame(rows)
+
+    # DataFrame with both MainStat and SubStat values (Messy Data)
+    artifactDF = artifactDF.dropna(subset=['Stat', 'Amount'])
+
+    # Seperating the mainstat values and the substat values into their own df
+    main_stats_df = artifactDF[~artifactDF['Artifact'].str.endswith(' ss')].copy()
+    substats_df = artifactDF[artifactDF['Artifact'].str.endswith(' ss')].copy()
+
+    # Removing the 'ss' since its already in a substat dataframe
+    substats_df['Artifact'] = substats_df['Artifact'].str.replace(' ss', '')
+
+    # Add IDs to the DataFrames (helps to identify it in a SQL DB)
+    main_stats_df.insert(0, 'Artifact ID', range(1, len(main_stats_df) + 1))
+    substats_df.insert(0, 'Substat ID', range(1, len(substats_df) + 1))
+
+    # Add the Artifact IDs to the substats DataFrame (helps to identify it in a SQL DB)
+    artifact_id_map = main_stats_df[['Artifact', 'Artifact ID']].set_index('Artifact').to_dict()['Artifact ID']
+    substats_df['Artifact ID'] = substats_df['Artifact'].map(artifact_id_map)
+
+    print(tabulate(main_stats_df, headers='keys', tablefmt='psql'))
+    print(tabulate(substats_df, headers='keys', tablefmt='psql'))
+    #------------------------------------------------------------#
+
+    # Weapons DF
+    weapon_info = asyncio.run(aF.weapon_Extractor(uid, character))
+    weapon_stat_df = pd.DataFrame(weapon_info, index=['Level', 'Refinement', 'Ascension'])
+    weapon_stat_df['Character'] = character
+    weaponDF = weapon_stat_df.reset_index()
+    columns = weaponDF.columns.tolist()
+
+    # Changing the columns to my liking
+    weaponDF = weaponDF[['Character'] + columns[:-1]]
+
+    # Rename the columns
+    weaponDF.columns = ['Character', 'Stat', 'Value']
+
+    # Add a column for the Weapon Name
+    weaponDF['Weapon'] = list(weapon_info.keys())[0]
+
+    print(tabulate(weaponDF, headers='keys', tablefmt='psql'))
+
+
+    #-------------------------------------------#
+    #              All Tables                   #
+    #                                           #
+    # - uidDF : players UID                     #
+    # - playerInfoDF : Info of Player           #
+    # - main_stats_df : main artifact stat info #
+    # - substats_df : artifact substat info     #
+    # - weaponDF : character Weapon Info        #
+    #-------------------------------------------#
+
 
 
 
